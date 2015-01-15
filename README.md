e4ae3fea36267384313b8de46360dadd78a7131e
Mannie Schumpert <mannieschumpert@gmail.com>
2015-01-15 09:39:19 -0500

    initial

---
+++ README.md
@@ -0,0 +1,28 @@
+# File Editor Control
+
+Allows you to control who has file editor capabilities in the WordPress admin.
+
+## Usage
+There's no UI. In your functions.php, add a filter that returns an array of user IDs that are allowed to edit files. By default, user ID 1 is allowed to edit files.
+
+Of course you can always just copy the plugin code and put it in your functions directly.
+
+# Examples
+Prevent all file editor access by passing an empty array:
+
+```php
+add_filter( 'fec_allowed_file_editors', 'my_allowed_file_editors' );
+function my_allowed_file_editors( $allowed_editors ){
+	return array( );
+}
+```
+
+Change allowed users:
+
+```php
+add_filter( 'fec_allowed_file_editors', 'my_allowed_file_editors' );
+function my_allowed_file_editors( $allowed_editors ){
+
+	return array( 2, 4 );
+}
+```
---
+++ file-editor-control.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Plugin Name:       File Editor Control
+ * Description:       Adds ability to allow only certain users access to the WordPress file editor
+ * Version:           1.0.0
+ * Author:            Mannie Schumpert
+ * Author URI:        http://mannieschumpert@gmail.com
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+    die;
+}
+
+add_filter( 'user_has_cap', 'file_editor_control', 10, 4 );
+function file_editor_control( $caps, $cap, $args, $user ){
+
+    $allowed_editors = array( 1 );
+    $allowed_editors = apply_filters( 'fec_allowed_file_editors', $allowed_editors );
+
+    // Don't block caps if current user = allowed user
+    if ( in_array( $user->ID, $allowed_editors ) )
+        return $caps;
+
+    $caps['edit_themes'] = false;
+
+    return $caps;
+}
